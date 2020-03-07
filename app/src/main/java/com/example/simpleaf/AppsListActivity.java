d774b89dbb0dceddf8cbbd82ddc75af28d89a5b2
mithun_babbira <mithunbabbira@gmail.com>
2020-03-07 18:06:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/simpleaf/AppsListActivity.java
@@ -0,0 +1,14 @@
+package com.example.simpleaf;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AppsListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_apps_list);
+    }
+}
---
+++ app/src/main/java/com/example/simpleaf/Item.java
@@ -0,0 +1,9 @@
+package com.example.simpleaf;
+
+import android.graphics.drawable.Drawable;
+
+public class Item {
+    CharSequence label;//package name
+    CharSequence name;//name
+    Drawable icon;//app icon
+}
